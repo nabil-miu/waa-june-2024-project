e84bd561440163e6998de500d7c4de7536f0f503
Mohamed Nabil <mohamedmenasy@gmail.com>
2024-06-29 02:35:43 +0300

    Initial Project Setup

---
+++ src/main/java/edu/miu/cs545/project/model/entity/User.java
@@ -0,0 +1,30 @@
+package edu.miu.cs545.project.model.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+public class User {
+    @Id
+    private Long id;
+    private String username;
+    private String password;
+    private String email;
+    private String firstName;
+    private String lastName;
+    private String phone;
+    private String address;
+    private String city;
+    private String state;
+    private String zip;
+    private String country;
+    private String role;
+    private boolean enabled;
+    private boolean accountNonExpired;
+    private boolean accountNonLocked;
+    private boolean credentialsNonExpired;
+}
