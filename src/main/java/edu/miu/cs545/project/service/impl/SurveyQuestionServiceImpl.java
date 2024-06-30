c7199b3d86205ca8a1d8754ad20260de47158132
BtsuanEyakem <btsuanme@gmail.com>
2024-06-29 22:51:31 -0500

    updated survey and SurveyRepo

--- src/main/java/edu/miu/cs545/project/service/impl/SurveyQuestionServiceImpl.java
+++ src/main/java/edu/miu/cs545/project/service/impl/SurveyQuestionServiceImpl.java
@@ -1,12 +1,13 @@
 package edu.miu.cs545.project.service.impl;
 
+import edu.miu.cs545.project.model.entity.SurveyQuestion;
 import edu.miu.cs545.project.repository.SurveyQuestionRepo;
 import edu.miu.cs545.project.service.SurveyQuestionService;
 import org.springframework.data.repository.ListCrudRepository;
 import org.springframework.stereotype.Service;
 
 @Service
-public class SurveyQuestionServiceImpl extends CrudServiceImpl implements SurveyQuestionService {
+public class SurveyQuestionServiceImpl extends CrudServiceImpl<SurveyQuestion,Long> implements SurveyQuestionService {
     public SurveyQuestionServiceImpl(SurveyQuestionRepo repository) {
         super(repository);
     }
