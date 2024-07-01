89fa5041566a4d8324a161c9ab6d895fdbbb06ee
Reza Un Nabi <reza.newgen@gmail.com>
2024-07-01 15:28:39 -0500

    Use ListCRUDRepository

--- src/main/java/edu/miu/cs545/project/repository/PostRepository.java
+++ src/main/java/edu/miu/cs545/project/repository/PostRepository.java
@@ -1,9 +1,9 @@
 package edu.miu.cs545.project.repository;
 
 import edu.miu.cs545.project.model.entity.Post;
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.ListCrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface PostRepository extends JpaRepository<Post,Long> {
+public interface PostRepository extends ListCrudRepository<Post,Long> {
 }
--- src/main/java/edu/miu/cs545/project/service/impl/PostServiceImpl.java
+++ src/main/java/edu/miu/cs545/project/service/impl/PostServiceImpl.java
@@ -1,20 +1,24 @@
 package edu.miu.cs545.project.service.impl;
 
 import edu.miu.cs545.project.model.entity.Post;
+import edu.miu.cs545.project.model.entity.ThreadPost;
 import edu.miu.cs545.project.repository.PostRepository;
 import edu.miu.cs545.project.repository.ThreadPostRepository;
 import edu.miu.cs545.project.service.PostService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.repository.ListCrudRepository;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+@Transactional
 @Service
-public class PostServiceImpl extends CrudServiceImpl<Post,Long> implements PostService {
-    public PostServiceImpl(PostRepository postRepository) {
-        super(postRepository);
+public class PostServiceImpl extends CrudServiceImpl<Post, Long> implements PostService {
+    public PostServiceImpl(ListCrudRepository<Post, Long> postRepository) {
+        super( postRepository);
     }
 
     @Autowired
-    private  PostRepository postRepository;
+    private PostRepository postRepository;
 
     @Autowired
     private ThreadPostRepository threadPostRepository;
@@ -22,11 +26,13 @@ public class PostServiceImpl extends CrudServiceImpl<Post,Long> implements PostS
     @Override
     public Post create(Post post) {
         //very first post
-        if(post.getParentPost() != null && post.getParentPost().getId() == null) {
+        if (post.getParentPost() != null && post.getParentPost().getId() == null) {
             // Ensure that the associated ThreadPost is saved
             if (post.getThreadPost() != null && post.getThreadPost().getId() == null) {
-                post.setThreadPost(threadPostRepository.save(post.getThreadPost()));
+                ThreadPost newThread = threadPostRepository.save(post.getThreadPost());
+                post.setThreadPost(newThread);
             }
+            post.setParentPost(null);
         }
         return postRepository.save(post);
     }
