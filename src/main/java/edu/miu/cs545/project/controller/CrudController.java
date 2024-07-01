d85230f0712065a46eca5bf7d50b181ba9c25074
Darkhan Zhursin <daratechn@gmail.com>
2024-07-01 08:10:20 -0500

    actuator, controller logging

--- src/main/java/edu/miu/cs545/project/controller/CrudController.java
+++ src/main/java/edu/miu/cs545/project/controller/CrudController.java
@@ -2,6 +2,8 @@ package edu.miu.cs545.project.controller;
 
 import edu.miu.cs545.project.service.CrudService;
 import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
@@ -14,23 +16,28 @@ import java.util.Optional;
 @RequiredArgsConstructor
 public abstract class CrudController<T, ID> {
 
+    private final Logger logger = LoggerFactory.getLogger(getClass());
+
     private final CrudService<T, ID> crudService;
 
     @GetMapping("/all")
-    public ResponseEntity<List<T>> getAll() {
+    public ResponseEntity<List<T>> getAll() throws NoSuchMethodException {
+        callLogger(getClass().getSimpleName(), getClass().getMethod("getAll", null));
         List<T> entities = crudService.getAll();
         return ResponseEntity.ok().body(entities);
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<T> getById(@PathVariable ID id) {
+    public ResponseEntity<T> getById(@PathVariable ID id) throws NoSuchMethodException {
+        callLogger(getClass().toString(), getClass().getMethod("getById", Object.class));
         Optional<T> optionalEntity = crudService.getById(id);
         return optionalEntity.map(ResponseEntity::ok)
                 .orElseGet(() -> ResponseEntity.notFound().build());
     }
 
     @PostMapping
-    public ResponseEntity<Void> create(@RequestBody T entity) {
+    public ResponseEntity<Void> create(@RequestBody T entity) throws NoSuchMethodException {
+        callLogger(getClass().getSimpleName(), getClass().getMethod("create", null));
         crudService.create(entity);
         URI location = ServletUriComponentsBuilder.fromCurrentRequestUri()
                 .path("/{id}")
@@ -40,13 +47,15 @@ public abstract class CrudController<T, ID> {
     }
 
     @PutMapping("/{id}")
-    public ResponseEntity<T> update(@PathVariable ID id, @RequestBody T entity) {
+    public ResponseEntity<T> update(@PathVariable ID id, @RequestBody T entity) throws NoSuchMethodException {
+        callLogger(getClass().getSimpleName(), getClass().getMethod("update", null));
         crudService.update(id, entity);
         return ResponseEntity.ok(entity);
     }
 
     @DeleteMapping("/{id}")
-    public ResponseEntity<Void> delete(@PathVariable ID id) {
+    public ResponseEntity<Void> delete(@PathVariable ID id) throws NoSuchMethodException {
+        callLogger(getClass().getSimpleName(), getClass().getMethod("delete", null));
         if (!crudService.existsById(id)) {
             System.out.println("Not found");
             return ResponseEntity.notFound().build();
@@ -62,5 +71,10 @@ public abstract class CrudController<T, ID> {
         } catch (Exception e) {
             throw new IllegalArgumentException("Failed to get ID from entity", e);
         }
+    }
+
+    private void callLogger(String className, Method method) {
+        logger.debug("Logging message within class: {}, method: {}", className,
+                method.getName());
     }
 }
