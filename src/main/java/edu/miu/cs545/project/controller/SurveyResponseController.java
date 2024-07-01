7be15b1777f7aa99fbb7725106fece0c960437a3
BtsuanEyakem <btsuanme@gmail.com>
2024-07-01 13:39:18 -0500

    surveyResponse

--- src/main/java/edu/miu/cs545/project/controller/SurveyResponseController.java
+++ src/main/java/edu/miu/cs545/project/controller/SurveyResponseController.java
@@ -2,6 +2,7 @@ package edu.miu.cs545.project.controller;
 
 import edu.miu.cs545.project.model.entity.SurveyResponse;
 import edu.miu.cs545.project.service.CrudService;
+import edu.miu.cs545.project.service.SurveyResponseService;
 import edu.miu.cs545.project.service.impl.SurveyResponseImpl;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,7 +12,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/v1/survey-responses")
 @Tag(name = "Survey Responses", description = "Survey Response API")
 public class SurveyResponseController extends CrudController<SurveyResponse,Long>{
-    public SurveyResponseController(SurveyResponseImpl Service) {
-        super(Service);
+
+
+    public SurveyResponseController(SurveyResponseService crudService) {
+        super(crudService);
     }
 }
