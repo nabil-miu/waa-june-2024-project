1cb8a294f716f1f3d4b62edd561b831146c2319d
BtsuanEyakem <btsuanme@gmail.com>
2024-07-01 20:14:27 -0500

    getActiveSuveys updated

--- src/main/java/edu/miu/cs545/project/service/impl/SurveyServiceImpl.java
+++ src/main/java/edu/miu/cs545/project/service/impl/SurveyServiceImpl.java
@@ -3,12 +3,26 @@ package edu.miu.cs545.project.service.impl;
 import edu.miu.cs545.project.model.entity.Survey;
 import edu.miu.cs545.project.repository.SurveyRepo;
 import edu.miu.cs545.project.service.SurveyService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.repository.ListCrudRepository;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
 @Service
 public class SurveyServiceImpl extends CrudServiceImpl<Survey,Long> implements SurveyService {
+    @Autowired
+    private SurveyRepo surveyRepo;
+
     public SurveyServiceImpl(SurveyRepo repository) {
         super(repository);
+    }
+
+    public List<Survey> getAllActiveSurveys() {
+        LocalDate date = LocalDate.now();
+        return surveyRepo.findByExpiredAtAfter(date);
     }
 }
