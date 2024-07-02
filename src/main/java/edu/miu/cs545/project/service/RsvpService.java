22d340fc6fdc752823342d7a344ecab6231bb658
Darkhan Zhursin <daratechn@gmail.com>
2024-07-01 20:26:28 -0500

    get all rsvps by event

--- src/main/java/edu/miu/cs545/project/service/RsvpService.java
+++ src/main/java/edu/miu/cs545/project/service/RsvpService.java
@@ -1,6 +1,12 @@
 package edu.miu.cs545.project.service;
 
+import edu.miu.cs545.project.model.entity.Event;
 import edu.miu.cs545.project.model.entity.Rsvp;
 
+import java.util.List;
+
 public interface RsvpService extends CrudService<Rsvp, Long> {
+
+    List<Rsvp> getAllRsvpsByEvent(Event event);
+
 }
--- src/main/java/edu/miu/cs545/project/service/impl/RsvpServiceImpl.java
+++ src/main/java/edu/miu/cs545/project/service/impl/RsvpServiceImpl.java
@@ -1,15 +1,25 @@
 package edu.miu.cs545.project.service.impl;
 
+import edu.miu.cs545.project.model.entity.Event;
 import edu.miu.cs545.project.model.entity.Rsvp;
 import edu.miu.cs545.project.repository.RsvpRepo;
 import edu.miu.cs545.project.service.RsvpService;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class RsvpServiceImpl extends CrudServiceImpl<Rsvp, Long> implements RsvpService {
 
+    private final RsvpRepo repository;
+
     public RsvpServiceImpl(RsvpRepo repository) {
         super(repository);
+        this.repository = repository;
     }
 
+    @Override
+    public List<Rsvp> getAllRsvpsByEvent(Event event) {
+        return repository.getRsvpsByEvent(event);
+    }
 }
